13d26e9a617cc6ab78ff9e3bbef026f8eea11351
Nahid <md.nahid186502@gmail.com>
2026-09-24 19:29:23 +0600

    My Plan page design done without functionalites

--- src/app/myPlan/page.tsx
+++ src/app/myPlan/page.tsx
@@ -2,8 +2,33 @@ import React from 'react';
 
 const MyPlanPage = () => {
     return (
-        <div>
-            <h2 className='text-4xl text-green-500 text-center font-bold py-5'>This is the My plan page</h2>
+        <div className='container mx-auto py-10 space-y-7 '>
+            <div className='space-y-2'>
+                <h3 className='text-2xl font-bold scale-y-150'>MY PLAN</h3>
+                <p>Cap of five lifts for today. Finish them, then load more.</p>
+            </div>
+
+            <div className='flex gap-[33.3%] bg-[#232732] px-5 py-10 rounded-2xl'>
+                <div>
+                    <p className='text-gray-400'>Exercises</p>
+                    <h3 className='text-3xl font-bold scale-y-120 text-[#C2F800]'>20</h3>
+                </div>
+                <div className='border-l pl-5 border-gray-600'>
+                    <p className='text-gray-400'>Minutes</p>
+                    <h3 className='text-3xl font-bold scale-y-120'>340</h3>
+                </div>
+                <div className='border-l pl-5 border-gray-600'>
+                    <p className='text-gray-400'>Calories</p>
+                    <h3 className='text-3xl font-bold scale-y-120'>230</h3>
+                </div>
+            </div>
+
+            <div>
+                <div className='border border-gray-400 px-4 py-3 rounded-2xl w-fit flex gap-6 bg-[#232732]'>
+                    <button>Today's Plan</button>
+                    <button>Saved</button>
+                </div>
+            </div>
         </div>
     );
 };
